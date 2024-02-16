9c7af94131973252b04abb858675a0015c9323d7
Muhammad Haris <mharis01470017@gmail.com>
2024-02-16 23:44:35 +0500

    hackathon-zero

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,20 +4,13 @@ import { CldUploadButton } from 'next-cloudinary';
 import { CldImage } from 'next-cloudinary';
 import { useState } from 'react';
 
-export type UploadResult = {
-  info: {
-    public_id: string;
-  };
-  event: "success";
-};
-
 export default function Home() {
   const [imageId, setImageId] = useState("")
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">  
       <CldUploadButton
-        onUpload={(result: UploadResult) => {
+        onUpload={(result: any) => {
           setImageId(result.info.public_id);
         }}
         uploadPreset="hackathon-zero"
